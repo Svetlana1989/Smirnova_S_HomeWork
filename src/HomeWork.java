cea37f16bd662df7929b69d488efc687ed09c49b
Svetlana <89168330523@mail.ru>
2021-02-27 20:54:11 +0300

    Домашнее задание за 5 урок

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,93 +1,93 @@
-import java.util.Scanner;
-
-public class HomeWork {
-
-    // 1.Создать пустой проект в IntelliJ IDEA и прописать метод main().
-    public static void main(String[] args) {
-        System.out.println("Задача 3");
-        System.out.println(ur1_3(1, 1, 1, 1));
-        System.out.println("Задача 4");
-        System.out.println(ur1_4(3, 4));
-        System.out.println("Задача 5");
-        ur1_5(10);
-        System.out.println("Задача 6");
-        System.out.println(ur1_6(10));
-        System.out.println("Задача 7");
-        ur1_7("Михаил");
-        System.out.println("Задача 8");
-        ur1_8(2020);
-
-        // 2.Создать переменные всех пройденных типов данных и инициализировать их значения.
-        // Целые числа
-        byte byteVar1 = 127;
-        byte byteVar2 = -128;
-        short shortVar1 = 32767;
-        short shortVar2 = -32768;
-        int intVar1 = 2000000000;
-        int intVar2 = (int) 2e9;
-        long longVar1 = (long) 1e18;
-        long longVar2 = 123123123L;
-        // Вещественные числа
-        float floatVar = 123.445f;
-        double doubleVar = 1.5d;
-        // Символы
-        char symbol1 = '\'';
-        char symbol2 = 'a';
-        // Правда/ложь
-        boolean boolVar1 = true;
-        boolean boolVar2 = false;
-    }
-
-    // 3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
-    // где a, b, c, d – аргументы этого метода, имеющие тип float.
-    public static float ur1_3(float a, float b, float c, float d) {
-        return (a * (b + (c / d)));
-    }
-
-    // 4. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит
-    // в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
-    public static boolean ur1_4(int a, int b) {
-        int c = a + b;
-        return ((20 >= c) && (c > 10));
-    }
-
-    // 5. Написать метод, которому в качестве параметра передается целое число, метод должен
-    // напечатать в консоль, положительное ли число передали или отрицательное.
-    // Замечание: ноль считаем положительным числом.
-    public static void ur1_5(int a) {
-        if (a >= 0) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
-
-    // 6. Написать метод, которому в качестве параметра передается целое число.
-    // Метод должен вернуть true, если число отрицательное,
-    // и вернуть false если положительное.
-    public static boolean ur1_6(int a) {
-        if (a < 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // 7. Написать метод, которому в качестве параметра передается строка,
-    // обозначающая имя. Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
-    public static void ur1_7(String name) {
-        System.out.println("Привет, " + name + "!");
-    }
-
-    // 8. * Написать метод, который определяет, является ли год високосным,и выводит сообщение в консоль.
-    // Каждый 4-й год является високосным,кроме каждого 100-го, при этом каждый 400-й – високосный.
-    public static void ur1_8(int a) {
-        if (a % 4 == 0) {
-            if (a % 100 == 0) {
-                if (a % 400 == 0) {System.out.println("Год високосный");}
-                else {System.out.println("Год не високосный");}
-            } else {System.out.println("Год високосный");}
-        } else {
-            System.out.println("Год не високосный");}
-    }
-}
+//import java.util.Scanner;
+//
+//public class HomeWork {
+//
+//    // 1.Создать пустой проект в IntelliJ IDEA и прописать метод main().
+//    public static void main(String[] args) {
+//        System.out.println("Задача 3");
+//        System.out.println(ur1_3(1, 1, 1, 1));
+//        System.out.println("Задача 4");
+//        System.out.println(ur1_4(3, 4));
+//        System.out.println("Задача 5");
+//        ur1_5(10);
+//        System.out.println("Задача 6");
+//        System.out.println(ur1_6(10));
+//        System.out.println("Задача 7");
+//        ur1_7("Михаил");
+//        System.out.println("Задача 8");
+//        ur1_8(2020);
+//
+//        // 2.Создать переменные всех пройденных типов данных и инициализировать их значения.
+//        // Целые числа
+//        byte byteVar1 = 127;
+//        byte byteVar2 = -128;
+//        short shortVar1 = 32767;
+//        short shortVar2 = -32768;
+//        int intVar1 = 2000000000;
+//        int intVar2 = (int) 2e9;
+//        long longVar1 = (long) 1e18;
+//        long longVar2 = 123123123L;
+//        // Вещественные числа
+//        float floatVar = 123.445f;
+//        double doubleVar = 1.5d;
+//        // Символы
+//        char symbol1 = '\'';
+//        char symbol2 = 'a';
+//        // Правда/ложь
+//        boolean boolVar1 = true;
+//        boolean boolVar2 = false;
+//    }
+//
+//    // 3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+//    // где a, b, c, d – аргументы этого метода, имеющие тип float.
+//    public static float ur1_3(float a, float b, float c, float d) {
+//        return (a * (b + (c / d)));
+//    }
+//
+//    // 4. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит
+//    // в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+//    public static boolean ur1_4(int a, int b) {
+//        int c = a + b;
+//        return ((20 >= c) && (c > 10));
+//    }
+//
+//    // 5. Написать метод, которому в качестве параметра передается целое число, метод должен
+//    // напечатать в консоль, положительное ли число передали или отрицательное.
+//    // Замечание: ноль считаем положительным числом.
+//    public static void ur1_5(int a) {
+//        if (a >= 0) {
+//            System.out.println("Число положительное");
+//        } else {
+//            System.out.println("Число отрицательное");
+//        }
+//    }
+//
+//    // 6. Написать метод, которому в качестве параметра передается целое число.
+//    // Метод должен вернуть true, если число отрицательное,
+//    // и вернуть false если положительное.
+//    public static boolean ur1_6(int a) {
+//        if (a < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    // 7. Написать метод, которому в качестве параметра передается строка,
+//    // обозначающая имя. Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+//    public static void ur1_7(String name) {
+//        System.out.println("Привет, " + name + "!");
+//    }
+//
+//    // 8. * Написать метод, который определяет, является ли год високосным,и выводит сообщение в консоль.
+//    // Каждый 4-й год является високосным,кроме каждого 100-го, при этом каждый 400-й – високосный.
+//    public static void ur1_8(int a) {
+//        if (a % 4 == 0) {
+//            if (a % 100 == 0) {
+//                if (a % 400 == 0) {System.out.println("Год високосный");}
+//                else {System.out.println("Год не високосный");}
+//            } else {System.out.println("Год високосный");}
+//        } else {
+//            System.out.println("Год не високосный");}
+//    }
+//}
--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,99 +1,99 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
-
-public class HomeWork2 {
-    public static void main(String[] args) {
-
-        // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
-        System.out.println("Задание 1");
-        System.out.println("1 1 0 0 1 0 1 1 0 0");
-        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else arr[i] = 0;
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-        // 2. Задать пустой целочисленный массив размером 8.
-        // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
-        System.out.println("Задание 2");
-        System.out.println("0 3 6 9 12 15 18 21");
-        int[] arr2 = new int[8];
-        arr2[0] = 0;
-        for (int i = 1; i < arr2.length; i++) {
-            arr2[i] = arr2[i - 1] + 3;
-        }
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
-        }
-        System.out.println();
-
-        // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
-        // и числа меньшие 6 умножить на 2;
-        System.out.println("Задание 3");
-        System.out.println("1 5 3 2 11 4 5 2 4 8 9 1");
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6) {
-                arr3[i] = arr3[i] * 2;
-            }
-            System.out.print(arr3[i] + " ");
-        }
-        System.out.println();
-
-        // 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое)
-        // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
-        System.out.println("Задание 4");
-        int[][] arr4 = {{14, 56, 13, 17}, {70, 0, 13, 12}, {8, 17, 24, 3}, {12, 6, 9, 8}};
-        for (int i = 0; i < arr4.length; i++) {
-            for (int j = 0; j < arr4.length; j++) {
-                if (i == j || i == arr4.length - 1 - j) {
-                    arr4[i][j] = 1;
-                } else {
-                }
-                System.out.printf("%4d", arr4[i][j]);
-            }
-            System.out.println();
-        }
-
-        // 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы
-        System.out.println("Задание 5");
-        System.out.println("1 5 3 2 11 4 5 2 4 8 9 1");
-        int[] arr5 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        int min = arr5[0];
-        int max = arr5[0];
-        for (int i = 0; i < arr5.length; i++) {
-            if (max <= arr5[i]) {
-                max = arr5[i];
-            }
-            if (min >= arr5[i]) {
-                min = arr5[i];
-            }
-        }
-        System.out.println(min);
-        System.out.println(max);
-
-        // 6. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
-        // метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-        // Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true,
-        // граница показана символами ||, эти символы в массив не входят.
-        System.out.println("Задание 6");
-        System.out.println("2 2 2 1 2 2 10 1");
-        int sum = 0;
-        int sumEnd = 0;
-        int[] arr6 = {2, 2, 2, 1, 2, 2, 10, 1};
-        for (int i = 0; i < arr6.length; i++) {
-            sum = sum + arr6[i];
-            sumEnd = IntStream.of(arr6).sum() - sum;
-            if (sum == sumEnd){
-                break;
-            }
-        }
-        System.out.println(sum);
-        System.out.println(sumEnd);
-    }
-}
-
+//import java.util.Scanner;
+//import java.util.stream.IntStream;
+//
+//public class HomeWork2 {
+//    public static void main(String[] args) {
+//
+//        // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+//        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+//        System.out.println("Задание 1");
+//        System.out.println("1 1 0 0 1 0 1 1 0 0");
+//        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == 0) {
+//                arr[i] = 1;
+//            } else arr[i] = 0;
+//            System.out.print(arr[i] + " ");
+//        }
+//        System.out.println();
+//
+//        // 2. Задать пустой целочисленный массив размером 8.
+//        // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+//        System.out.println("Задание 2");
+//        System.out.println("0 3 6 9 12 15 18 21");
+//        int[] arr2 = new int[8];
+//        arr2[0] = 0;
+//        for (int i = 1; i < arr2.length; i++) {
+//            arr2[i] = arr2[i - 1] + 3;
+//        }
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.print(arr2[i] + " ");
+//        }
+//        System.out.println();
+//
+//        // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+//        // и числа меньшие 6 умножить на 2;
+//        System.out.println("Задание 3");
+//        System.out.println("1 5 3 2 11 4 5 2 4 8 9 1");
+//        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+//        for (int i = 0; i < arr3.length; i++) {
+//            if (arr3[i] < 6) {
+//                arr3[i] = arr3[i] * 2;
+//            }
+//            System.out.print(arr3[i] + " ");
+//        }
+//        System.out.println();
+//
+//        // 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое)
+//        // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+//        System.out.println("Задание 4");
+//        int[][] arr4 = {{14, 56, 13, 17}, {70, 0, 13, 12}, {8, 17, 24, 3}, {12, 6, 9, 8}};
+//        for (int i = 0; i < arr4.length; i++) {
+//            for (int j = 0; j < arr4.length; j++) {
+//                if (i == j || i == arr4.length - 1 - j) {
+//                    arr4[i][j] = 1;
+//                } else {
+//                }
+//                System.out.printf("%4d", arr4[i][j]);
+//            }
+//            System.out.println();
+//        }
+//
+//        // 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы
+//        System.out.println("Задание 5");
+//        System.out.println("1 5 3 2 11 4 5 2 4 8 9 1");
+//        int[] arr5 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+//        int min = arr5[0];
+//        int max = arr5[0];
+//        for (int i = 0; i < arr5.length; i++) {
+//            if (max <= arr5[i]) {
+//                max = arr5[i];
+//            }
+//            if (min >= arr5[i]) {
+//                min = arr5[i];
+//            }
+//        }
+//        System.out.println(min);
+//        System.out.println(max);
+//
+//        // 6. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
+//        // метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+//        // Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true,
+//        // граница показана символами ||, эти символы в массив не входят.
+//        System.out.println("Задание 6");
+//        System.out.println("2 2 2 1 2 2 10 1");
+//        int sum = 0;
+//        int sumEnd = 0;
+//        int[] arr6 = {2, 2, 2, 1, 2, 2, 10, 1};
+//        for (int i = 0; i < arr6.length; i++) {
+//            sum = sum + arr6[i];
+//            sumEnd = IntStream.of(arr6).sum() - sum;
+//            if (sum == sumEnd){
+//                break;
+//            }
+//        }
+//        System.out.println(sum);
+//        System.out.println(sumEnd);
+//    }
+//}
+//
